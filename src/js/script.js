4b4e195daa80c7049c6dc5f6b610479ed64e0ed3
Brandon Leichty <brandonleichty@Brandons-MacBook-Pro.local>
2017-12-18 19:48:27 -0600

    gets JSON data from Treehouse and plots a doughnut chart

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,34 @@ const treehouseURL = 'https://teamtreehouse.com/brandonleichty.json';
 $.getJSON(treehouseURL, (data) => {
     console.log(data.name);
     console.log(data.points.HTML);
+
+
+    const CHART = document.getElementById("doughnutChart");
+
+    Chart.defaults.scale.ticks.beginAtZero = true;
+
+    let doughnutChart = new Chart(CHART, {
+        type: 'doughnut',
+        data: {
+            labels: ['HTML', 'CSS', 'Design', 'JavaScript', 'Development Tools'],
+            datasets: [
+                {
+                    label: 'Points scored by Vikings',
+                    backgroundColor: ['#2E112D', '#540032', '#820333', '#C9283E', '#F0433A'],
+                    data: [data.points.HTML, data.points.CSS, data.points.Design, data.points.JavaScript, data.points['Development Tools']]
+                }
+            ]
+        },  
+        options: {
+            cutoutPercentage: 80,
+            legend: {
+                display: false,
+            },
+            title: {
+                display: true,
+                text: `${data.name}'s Treehouse Points`
+            }
+        }
+    });
+
 });
