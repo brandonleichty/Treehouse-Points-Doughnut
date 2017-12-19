412f4c0fbb1a371b63cbeca09b24e176f008cfad
Brandon Leichty <brandonleichty@Brandons-MacBook-Pro.local>
2017-12-19 13:16:56 -0600

    doughnut is successfully rendered.

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,7 @@ Chart.defaults.scale.ticks.beginAtZero = true;
 //----------------
 
 
-const treehouseURL = 'https://teamtreehouse.com/brandonleichty.json';
+const treehouseURL = 'https://teamtreehouse.com/chalkers.json';
 
 const pointsPromise = fetch(treehouseURL);
 
@@ -16,42 +16,47 @@ pointsPromise
         .then(data => data.json()) //convert data to JSON
         .then(data => { 
             const pointsArray = [];
+            const skillsArray = [];
             for (const [key, value] of Object.entries(data.points)) {
-                if(value > 0) {
-                    pointsArray.push( {key, value} ); //push skills that have more than 0 points into 'pointsArray'
+                if(value > 0 && key != 'total') {
+                    pointsArray.push(value); //push skills that have more than 0 points into 'pointsArray'
+                    skillsArray.push(key) 
                 }
-                console.log(`${key} ${value}`);
               }
-              console.table(pointsArray);
+              console.log(skillsArray);
+              console.log(pointsArray);
+              renderDoughnut(skillsArray, pointsArray, data.name);
             })
         .catch((err) => {
             console.error(err);
         });
 
 
-    // let doughnutChart = new Chart(CHART, {
-    //     type: 'doughnut',
-    //     data: {
-    //         labels: ['HTML', 'CSS', 'Design', 'JavaScript', 'Development Tools'],
-    //         datasets: [
-    //             {
-    //                 label: 'Points scored by Vikings',
-    //                 backgroundColor: ['#2E112D', '#540032', '#820333', '#C9283E', '#F0433A'],
-    //                 data: [data.points.HTML, data.points.CSS, data.points.Design, data.points.JavaScript, data.points['Development Tools']]
-    //             }
-    //         ]
-    //     },  
-    //     options: {
-    //         cutoutPercentage: 70,
-    //         legend: {
-    //             display: false,
-    //         },
-    //         title: {
-    //             display: true,
-    //             text: `${data.name}'s Treehouse Points`
-    //         }
-    //     }
-    // });
+
+    function renderDoughnut(skillLabels, pointsData, userName) {
+    let doughnutChart = new Chart(CHART, {
+        type: 'doughnut',
+        data: {
+            labels: skillLabels,
+            datasets: [
+                {
+                    backgroundColor: ['#2E112D', '#540032', '#820333', '#C9283E', '#F0433A', '#02547D', '#0284A8', '#02BEC4', '#A9E8DC', '#E1F7E7', '#734E67', '#5A3651', '#59584D', '#F1DEBC', '#D9CDBC'],
+                    data: pointsData
+                }
+            ]
+        },  
+        options: {
+            cutoutPercentage: 70,
+            legend: {
+                display: false,
+            },
+            title: {
+                display: true,
+                text: `${userName}'s Treehouse Points`
+            }
+        }
+    });
+}
 
 
 
